74a7f2083a4fdc63675d73e3695c51a4ab538c0f
Alejandro Pinto <pinto.alex05@gmail.com>
2024-10-21 14:41:13 -0400

    Finished implementing the island functionality

--- src/Cluster.py
+++
@@ -1,7 +0,0 @@
-"""
-This cluster idea is borrowed from the FunSearch paper.
-The idea is that we want to randomly sample by
-"""
-class Cluster:
-    def __init__(self) -> None:
-
--- src/Island.py
+++ src/Island.py
@@ -1,13 +1,18 @@
 from scipy.special import softmax
 import numpy as np
 
+from dataclasses import dataclass
+
 """
 Contains a (function, score) pair.
 """
+@dataclass
 class Sample:
-    def __init__(self, function: str, score: float) -> None:
-        self.function: str = function
-        self.score: float = score
+    function: str = ""
+    score: float = 0
+
+    def __repr__(self) -> str:
+        return str(self.score)
 
 
 """
@@ -17,13 +22,37 @@ score is, and how simple it is.
 """
 class Island:
     def __init__(self) -> None:
-        self.population = []
+        self.population: list[Sample] = []
+        self.best_sample: Sample = Sample()
+        self.size: int = 0
 
     def get_sample(self) -> Sample:
         scores = [sample.score for sample in self.population]
         probs = softmax(scores)
-        print(type(probs))
-        return Sample("", 1.0)
+
+        index = np.random.choice(self.size, 1, p=probs)[0]
+        sample = self.population[index]
+
+        return sample
+    
+    def get_uniform_random_sample(self) -> Sample:
+        index = np.random.choice(self.size, 1)[0]
+        sample = self.population[index]
+
+        return sample
+    
+    def add_sample(self, sample: Sample) -> None:
+        self.population.append(sample)
+        self.best_sample = max(self.best_sample, sample, key=lambda x: x.score)
+        self.size += 1
+
+    def reset_population(self) -> None:
+        self.population = []
+        self.best_sample = Sample()
+        self.size = 0
+
+    def __repr__(self) -> str:
+        return str(self.population)
 
 
 if __name__ == "__main__":
@@ -33,4 +62,7 @@ if __name__ == "__main__":
     island.population.append(Sample("", -1.0))
     island.population.append(Sample("", -2.0))
 
-    island.get_sample()
+    print(island.get_sample())
+    print(island.get_sample())
+    print(island.get_sample())
+    print(island.get_sample())
